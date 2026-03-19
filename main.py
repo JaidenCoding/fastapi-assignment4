c240c92e58ed6dc2bdfca19d2e3243530527f5b8
Jaiden Jackson <jjack221@charlotte.edu>
2026-03-19 17:43:21 -0400

    feat: add root GET endpoint

--- main.py
+++ main.py
@@ -14,18 +14,3 @@ class Item(BaseModel):
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-# Part 1: GET with path + query parameter
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-# Part 2: PUT request
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {
-        "item_name": item.name,
-        "item_price": item.price,
-        "is_offer": item.is_offer,
-        "item_id": item_id
-    }
